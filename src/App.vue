8ef956b4abd5fa8bcb9a58b6e8d147d2cb3fc8fd
Alexandre Cédrick <alexandre.cedrick@gmail.com>
2021-05-10 10:46:11 +0200

    Install vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,87 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+
+      <v-bottom-navigation v-model="value">
+        <v-btn value="recent">
+          <span>Planning</span>
+
+          <v-icon>mdi-calendar-text</v-icon>
+        </v-btn>
+
+        <v-btn value="favorites">
+          <span>Dashboard</span>
+
+          <v-icon>mdi-view-dashboard</v-icon>
+        </v-btn>
+
+        <v-btn value="nearby">
+          <span>Alert</span>
+
+          <v-icon>mdi-alert</v-icon>
+        </v-btn>
+
+        <v-btn value="nearby">
+          <span>Event</span>
+
+          <v-icon>mdi-information</v-icon>
+        </v-btn>
+
+        <v-btn value="nearby">
+          <span>Quality</span>
+
+          <v-icon>mdi-check-all</v-icon>
+        </v-btn>
+      </v-bottom-navigation>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'App',
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
