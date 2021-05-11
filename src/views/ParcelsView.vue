ef9db6905a3a2d7f227def03c96898c1067d8a75
Alexandre Cédrick <alexandre.cedrick@gmail.com>
2021-05-11 15:23:52 +0200

    Add the capabilty to see products from a parcel

--- src/views/ParcelsView.vue
+++ src/views/ParcelsView.vue
@@ -20,22 +20,92 @@
                 <th class="text-left">
                   deliveryIndicator
                 </th>
+                <th class="text-left">
+                  Action
+                </th>
               </tr>
             </thead>
 
             <tbody>
               <tr
-                v-for="parcel in parcels"
+                v-for="(parcel, index) in parcels"
                 :key="parcel.serial"
               >
                 <td>{{ parcel.parcel_id }}</td>
                 <td>{{ parcel.order_id }}</td>
                 <td>{{ parcel.products.length }}</td>
                 <td>{{ parcel.deliveryIndicator }}</td>
+                <td><v-btn @click="seeMore(index)">
+                  <v-icon>mdi-eye</v-icon></v-btn></td>
               </tr>
             </tbody>
           </template>
         </v-simple-table>
+      </v-col>
+    </v-row>
+
+    <v-row v-if="parcelTemp !== null">
+      <v-col>
+        <v-card>
+          <v-card-title>{{parcelTemp.parcel_id}}</v-card-title>
+          <v-card-text>
+            <v-row>
+              <v-col cols="3">
+                <v-text-field
+                  label="order_id"
+                  disabled
+                  v-model="parcelTemp.order_id"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="3">
+                <v-text-field
+                  label="deliveryIndicator"
+                  disabled
+                  v-model="parcelTemp.deliveryIndicator"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="3">
+                <v-text-field
+                  label="weight"
+                  disabled
+                  v-model="parcelTemp.weight"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+
+            <v-row>
+              <v-col>
+                <v-simple-table>
+                  <template v-slot:default>
+                    <thead>
+                      <tr>
+                        <th class="text-left">
+                          Name
+                        </th>
+                        <th class="text-left">
+                          Serial
+                        </th>
+                        <th class="text-left">
+                          State
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr
+                        v-for="product in parcelTemp.products"
+                        :key="product.serial"
+                      >
+                        <td>{{ product.product_name }}</td>
+                        <td>{{ product.product_serial }}</td>
+                        <td>{{ product.product_state }}</td>
+                      </tr>
+                    </tbody>
+                  </template>
+                </v-simple-table>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -44,9 +114,21 @@
 <script>
 export default {
   name: 'ParcelView',
+  data () {
+    return {
+      parcelTemp: null,
+      products: []
+    }
+  },
   computed: {
     parcels () {
       return this.$store.state.parcels
+    },
+    
+  },
+  methods: {
+    seeMore (index) {
+      this.parcelTemp = this.parcels[index]
     }
   }
 }
